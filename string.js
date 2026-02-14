78a4a356c5a3575940d87e06d3fbea26596caf97
Tharun Reddy <tharunreddysudhini325@gmail.com>
2026-02-14 23:28:16 +0530

    second commit

--- string.js
+++ string.js
@@ -1,6 +1,9 @@
-function reverseString(str) {
-    return str.split("").reverse().join("");
-}
 
-console.log(reverseString("hello")); // "olleh"
-
+// declare the variables
+let x=5;
+let y=44;
+let z=x+y;
+console.log(z);  //here it prints the output - 49
+//
+let k="hello buddy";
+console.log(typeof k);   // here the o/p is string
